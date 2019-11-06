621b3a66c78f773d0b842bed8b089dfcf6313b15
Calob Humble <cshumble@protonmail.com>
2019-11-06 14:48:08 -0500

    Setting up the controller state

---
+++ README.md
@@ -0,0 +1,22 @@
+# Southport Robo Command and Control
+
+> Control robots with a DS4 controller and a cellphone!
+
+This demo will serve as a jumping off pointer for building a command-and-control system for remote deployed robots, allowing for flexible systems to be added on top of it.
+
+## Requirements
+
+1. NodeJS >= 12.13 LTS [Link](https://nodejs.org/en/)
+2. A USB or Bluetooth controller (e.g. Dualshock 4 controller)
+
+## Installation
+
+```bash
+npm install
+```
+
+## Deployment
+
+```bash
+npm run server
+```
---
+++ web-client/client.js
@@ -0,0 +1,20 @@
+// First lets define our controllers states.  For this example, I'll encode
+// the four main buttons, the right trigger, and the right joy sticks.
+let controller = {
+  buttons: {
+    x: false,
+    circle: false,
+    square: false,
+    tiangle: false,
+    right_trigger: false
+  },
+  // While our buttons can only be "pressed" or "not pressed" we need to store our
+  // joy sticks as a vector, split into x and y directions, so we can derive it's location
+  joysticks: {
+    right: {
+      x: 0,
+      y: 0
+    }
+  }
+};
+
